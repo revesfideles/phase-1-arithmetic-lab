dbcc0157e3dfbbb9d8faa39e54eb6f6d714bc326
revesfideles <revesfideles@gmail.com>
2024-02-18 11:33:35 -0500

    :Completed lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) +1;
+console.log(random);
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+let numbers = [5, 10, 15, 20];
+let max = Math.max(...numbers);
+console.log(max);
